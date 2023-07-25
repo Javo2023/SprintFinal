3c7836b329891db2972c1f69ca66e7cc5ba7f81c
javo2023 <jguzmanalarcon@gmail.com>
2023-07-25 11:13:19 -0400

    Se corrigen errores, vista de detalle ok

--- app/src/main/java/com/example/sprintfinal/Adapter.kt
+++ app/src/main/java/com/example/sprintfinal/Adapter.kt
@@ -1,46 +1,27 @@
 package com.example.sprintfinal
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.net.toUri
-import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
-import com.example.sprintfinal.databinding.ActivityMainBinding
 import com.example.sprintfinal.databinding.ItemLayoutBinding
 
-class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>(){
+class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
     var shoesList = mutableListOf<Shoes>()
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = shoesList[position]
         holder.bind(item)
 
-        holder.itemView.setOnClickListener {
-            // Obtener el NavController desde el Fragmento actual
-            val navController = findNavController(it)
 
-            // Navegar al FragmentDetalle y pasar datos como argumentos
-
-                val bundle = Bundle().apply {
-                    putString("nombre", item.nombre)
-                    putString("precio", item.precio.toString())
-                    putString("url", item.url)
-                    putString("detalle",item.detalle)
-
-                }
-            navController.navigate(R.id.action_shoesListFragment_to_fragmentDetail, bundle)
-
-        }
     }
 
-
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -48,22 +29,30 @@ class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>(){
         return shoesList.size
     }
 
-    fun setData(shoeList: List<Shoes>){
+    fun setData(shoeList: List<Shoes>) {
         this.shoesList = shoeList.toMutableList()
     }
 
-
-    class ViewHolder (val binding: ItemLayoutBinding): RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(shoe: Shoes) {
             binding.tvNombre.text = shoe.nombre
             binding.tvPrecio.text = shoe.precio.toString()
             binding.imageView.load(shoe.url)
 
+            binding.cvData.setOnClickListener {
+          Log.d("Adapter", "Item clicked: ${shoe.nombre}")
+          // Obtener el NavController desde el Fragmento actual
+
+
+          // Navegar al FragmentDetail y pasar datos como argumentos
+          val bundle = Bundle().apply {
+              putString("nombre", shoe.nombre)
+              putString("precio", shoe.precio.toString())
+              putString("url", shoe.url)
+              putString("detalle", shoe.detalle)
+          }
+         Navigation.findNavController(binding.root).navigate(R.id.action_shoesListFragment_to_fragmentDetail, bundle)
+      }
+        }
     }
-
-
-}
-
-
-
 }
--- app/src/main/java/com/example/sprintfinal/DetailFragment.kt
+++ app/src/main/java/com/example/sprintfinal/DetailFragment.kt
@@ -13,11 +13,6 @@ import com.example.sprintfinal.databinding.FragmentShoesListBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "nombre"
-private const val ARG_PARAM2 = "precio"
-private const val ARG_PARAM3 = "url"
-private const val ARG_PARAM4 = "detalle"
-
 
 
 /**
@@ -26,19 +21,14 @@ private const val ARG_PARAM4 = "detalle"
  * create an instance of this fragment.
  */
 class DetailFragment : Fragment() {
-    fun onSomeButtonClick() {
-        findNavController().navigate(R.id.action_shoesListFragment_to_fragmentDetail)
-    }
-
-
-    lateinit var binding : FragmentDetailBinding //
+      lateinit var binding : FragmentDetailBinding //
 
     var adapter = Adapter()
     // TODO: Rename and change types of parameters
-    private var param1: String? = "nombre"
-    private var param2: String? = "precio"
-    private var param3: String? = "url"
-    private var param4: String? = "detalle"
+    private var param1: String? = ""
+    private var param2: String? = ""
+    private var param3: String? = ""
+    private var param4: String? = ""
 
 
 
@@ -47,33 +37,23 @@ class DetailFragment : Fragment() {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString("nombre")
-            param2 = it.getString("precio")
-            param3 = it.getString("url")
+            param2 = it.getString("url")
+            param3 = it.getString("precio")
             param4 = it.getString("detalle")
 
         }
 
     }
-    fun newInstance(param1: String, param2: String,param3: String, param4: String): DetailFragment {
-        val fragment = DetailFragment()
-        val args = Bundle()
-        args.putString(ARG_PARAM1, param1)
-        args.putString(ARG_PARAM2, param2)
-        args.putString(ARG_PARAM3, param3)
-        args.putString(ARG_PARAM4, param4)
-        fragment.arguments = args
-        return fragment
-    }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
-        Glide.with(binding.root).load(param3).into(binding.ivDetail)
+        Glide.with(binding.root).load(param2).into(binding.ivDetail)
         binding.tvNombre1.text = param1
-
+        binding.tvDetail.text = param4
+        binding.tvPrecio1.text = param3
 
 
 
@@ -81,25 +61,5 @@ class DetailFragment : Fragment() {
         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentDetail.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                    putString(ARG_PARAM3, param3)
-                    putString(ARG_PARAM4, param4)
-                }
-            }
-    }
+
 }
